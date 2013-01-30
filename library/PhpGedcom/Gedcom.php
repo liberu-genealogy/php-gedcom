d0b3dc86ab0d1c1c67f3a774214529446033c0c3
Kristopher Wilson <kristopherwilson@gmail.com>
2013-01-29 21:34:40 -0500

    adding phpdox documentation

--- library/PhpGedcom/Gedcom.php
+++ library/PhpGedcom/Gedcom.php
@@ -21,51 +21,70 @@ namespace PhpGedcom;
 class Gedcom
 {
     /**
+     * Stores the header information of the GEDCOM file.
+     *
      * @var \PhpGedcom\Record\Head
      */
     protected $_head;
 
     /**
+     * Stores the submission information for the GEDCOM file.
+     *
      * @var \PhpGedcom\Record\Subn
      */
     protected $_subn;
 
     /**
+     * Stores sources cited throughout the GEDCOM file.
+     *
      * @var array
      */
     protected $_sour = array();
 
     /**
+     * Stores all the individuals contained within the GEDCOM file.
+     *
      * @var array
      */
     protected $_indi = array();
 
     /**
+     * Stores all the families contained within the GEDCOM file.
+     *
      * @var array
      */
     protected $_fam  = array();
 
     /**
+     * Stores all the notes contained within the GEDCOM file that are not inline.
+     *
      * @var array
      */
     protected $_note = array();
 
     /**
+     * Stores all repositories that are contained within the GEDCOM file and referenced by sources.
+     *
      * @var array
      */
     protected $_repo = array();
 
     /**
+     * Stores all the media objects that are contained within the GEDCOM file.
+     *
      * @var array
      */
     protected $_obje = array();
 
     /**
+     * Stores information about all the submitters to the GEDCOM file.
+     *
      * @var array
      */
     protected $_subm = array();
 
     /**
+     * Retrieves the header record of the GEDCOM file.
      *
      * @param Record\Head $head
      */
@@ -75,63 +94,79 @@ class Gedcom
     }
 
     /**
+     * Retrieves the submission record of the GEDCOM file.
+     *
      * @param Record\Subn $subn
      */
     public function setSubn(\PhpGedcom\Record\Subn $subn)
     {
         $this->_subn = $subn;
     }
-    
+
     /**
+     * Adds a source to the collection of sources.
      *
+     * @param Record\Sour $sour
      */
     public function addSour(\PhpGedcom\Record\Sour $sour)
     {
         $this->_sour[$sour->getId()] = $sour;
     }
-    
+
     /**
+     * Adds an individual to the collection of individuals.
      *
+     * @param Record\Indi $indi
      */
     public function addIndi(\PhpGedcom\Record\Indi $indi)
     {
         $this->_indi[$indi->getId()] = $indi;
     }
-    
+
     /**
+     * Adds a family to the collection of families.
      *
+     * @param Record\Fam $fam
      */
     public function addFam(\PhpGedcom\Record\Fam $fam)
     {
         $this->_fam[$fam->getId()] = $fam;
     }
-    
+
     /**
+     * Adds a note to the collection of notes.
      *
+     * @param Record\Note $note
      */
     public function addNote(\PhpGedcom\Record\Note $note)
     {
         $this->_note[$note->getId()] = $note;
     }
-    
+
     /**
+     * Adds a repository to the collection of repositories.
      *
+     * @param Record\Repo $repo
      */
     public function addRepo(\PhpGedcom\Record\Repo $repo)
     {
         $this->_repo[$repo->getId()] = $repo;
     }
-    
+
     /**
+     * Adds an object to the collection of objects.
      *
+     * @param Record\Obje $obje
      */
     public function addObje(\PhpGedcom\Record\Obje $obje)
     {
         $this->_obje[$obje->getId()] = $obje;
     }
-    
+
     /**
+     * Adds a submitter record to the collection of submitters.
      *
+     * @param Record\Subm $subm
      */
     public function addSubm(\PhpGedcom\Record\Subm $subm)
     {
@@ -139,6 +174,7 @@ class Gedcom
     }
 
     /**
+     * Gets the header information of the GEDCOM file.
      *
      * @return Record\Head
      */
@@ -148,6 +184,8 @@ class Gedcom
     }
 
     /**
+     * Gets the submission record of the GEDCOM file.
+     *
      * @return Record\Subn
      */
     public function getSubn()
@@ -156,6 +194,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of submitters to the GEDCOM file.
      *
      * @return array
      */
@@ -165,6 +204,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of individuals stored in the GEDCOM file.
      *
      * @return array
      */
@@ -174,6 +214,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of families stored in the GEDCOM file.
      *
      * @return array
      */
@@ -183,6 +224,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of repositories stored in the GEDCOM file.
      *
      * @return array
      */
@@ -192,6 +234,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of sources stored in the GEDCOM file.
      *
      * @return array
      */
@@ -201,6 +244,7 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of note stored in the GEDCOM file.
      *
      * @return array
      */
@@ -210,34 +254,12 @@ class Gedcom
     }
 
     /**
+     * Gets the collection of objects stored in the GEDCOM file.
      *
      * @return array
      */
     public function getObje()
     {
         return $this->_obje;
-    }
-    
-    /**
-     *
-     * @throws \Exception Whenever called
-     * @param string $name Ignored
-     * @param string $value Ignored
-     */
-    public function __set($name, $value)
-    {
-        // prevent setting undefined attributes and not reporting the error
-        throw new \Exception('Undefined property ' . $name . ' in __set');
-    }
-    
-    /**
-     * 
-     * @throws \Exception Whenever called
-     * @param string $name Ignored
-     */
-    public function __get($name)
-    {
-        // prevent getting undefined attributes and not reporting the error
-        throw new \Exception('Undefined property ' . $name . ' in __get');
     }
 }
