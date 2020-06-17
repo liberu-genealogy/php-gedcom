4d038c8f11475a70bb9726517cae20b8d5831973
jyyblue <wenqiangliu344@gmail.com>
2020-06-17 06:49:36 -0700

    add Sour related writer.

---
+++ library/PhpGedcom/Writer/Repo.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * php-gedcom
+ *
+ * php-gedcom is a library for parsing, manipulating, importing and exporting
+ * GEDCOM 5.5 files in PHP 5.3+.
+ *
+ * @author          Xiang Ming <wenqiangliu344@gmail.com>
+ * @copyright       Copyright (c) 2010-2013, Xiang Ming
+ * @package         php-gedcom 
+ * @license         GPL-3.0
+ * @link            http://github.com/mrkrstphr/php-gedcom
+ */
+
+namespace PhpGedcom\Writer;
+
+/**
+ *
+ */
+class Repo
+{
+    /**
+     * @param \PhpGedcom\Record\Repo $sour
+     * @param int $level
+     * @return string
+     */
+    public static function convert(\PhpGedcom\Record\Repo &$repo, $level)
+    {
+        
+        $output = "";
+        return $output;
+    }
+}
