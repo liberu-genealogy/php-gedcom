8b8652e018c8ca21a8f48d0700ddda3458a5ec1f
jyyblue <wenqiangliu344@gmail.com>
2020-06-18 03:14:37 -0700

    add fam and realted writers

---
+++ library/PhpGedcom/Writer/Fam/Even/Husb.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * php-gedcom
+ *
+ * php-gedcom is a library for parsing, manipulating, importing and exporting
+ * GEDCOM 5.5 files in PHP 5.3+.
+ *
+ * @author          Xiang Ming <wenqiangliu344@gmail.com>
+ * @copyright       Copyright (c) 2010-2013, Xiang Ming
+ * @package         php-gedcom 
+ * @license         GPL-3.0
+ * @link            http://github.com/mrkrstphr/php-gedcom
+ */
+
+namespace PhpGedcom\Writer\Fam\Even;
+
+/**
+ *
+ */
+class Husb
+{
+    /**
+     * @param \PhpGedcom\Record\Fam\Even\Husb $attr
+     * @param int $level
+     * @return string
+     */
+    public static function convert(\PhpGedcom\Record\Fam\Even\Husb &$husb, $level = 0)
+    {
+        $output = '';
+        
+        $output.= $level." WIFE \n";
+        // level up
+        $level++;
+        
+        // AGE
+        $age = $husb->getAge();
+        if(!empty($age)){
+            $output.=$level." AGE ".$age."\n";
+        }
+        
+        return $output;
+    }
+}
