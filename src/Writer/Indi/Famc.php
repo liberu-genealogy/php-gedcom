e9b82b3f54d151c8b179e0ec1b1a60835af5ca71
Alba Rincón <alba@rincon.me>
2024-08-21 19:07:34 +0200

    Fix `Famc` class trying to access `Fams` and `Sour` methods

--- src/Writer/Indi/Famc.php
+++ src/Writer/Indi/Famc.php
@@ -27,8 +27,8 @@ class Famc
     {
         $output = '';
         // NAME
-        $_famc = $famc->getFams();
-        if (empty($_fams)) {
+        $_famc = $famc->getFamc();
+        if (empty($_famc)) {
             return $output;
         }
         $output .= $level.' FAMC @'.$_famc."@\n";
@@ -42,7 +42,7 @@ class Famc
         }
 
         // note
-        $note = $famc->getSour();
+        $note = $famc->getNote();
         if (!empty($note) && (is_countable($note) ? count($note) : 0) > 0) {
             foreach ($note as $item) {
                 $_convert = \Gedcom\Writer\NoteRef::convert($item, $level);
